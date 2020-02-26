5effdac4b7dd89d72b9fa68d35c862a43e27653f
Facundo3228 <esteban-r-m@hotmail.es>
2020-02-26 16:58:35 -0300

    Trabajo en casa

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -17,22 +17,61 @@ function mostrar()
     var notaMenor; //comparaciones
     var notaMayor; //comparaciones
     var sexoNotaMenor; //sexo de la nota menor
+    var sexoNotaMayor; //sexo de la nota mayor
     var contadorVaronesNotaMayor = 0; //La cantidad de varones que su nota haya sido mayor o igual a 6.
     var promedioNotas; //promedio de las notas totales 
     var acumuladorNotas = 0; //sum de cada nota ingresada
     var primeraIteracion = true; //bandera 
-    var mensaje; //mensajes
+    //var mensaje; //mensajes
 
     //while de 5 alumnos
-    while (contdor < 5) {
+    while (contador < 5) {
         contador++;
 
+        //validacion para las notas
         do {
+            //solisito la nota
             notaAlumno = prompt("Ingrese la nota del alumno: " + contador);
+            //parseo la variable
             notaAlumno = parseFloat(notaAlumno);
-        } while (isNaN(notaAlumno) || notaAlumno < -1 || notaAlumno > 11);
-        
+        } while (isNaN(notaAlumno) || notaAlumno < 0 || notaAlumno > 10);
+
+        do {
+            //solisito el sexo
+            sexoAlumno = prompt("Ingrese el sexo de la nota del alumno: " + contador);
+        } while (sexoAlumno.toLowerCase() != "f" && sexoAlumno.toLowerCase() != "m");
+
+        if (primeraIteracion) {
+            primeraIteracion = false;
+
+            notaMayor = notaAlumno;
+            notaMenor = notaAlumno;
+            sexoNotaMenor = sexoAlumno;
+            sexoNotaMayor = sexoAlumno;
+        }
+
+        if (notaMayor < notaAlumno) {
+            notaMayor = notaAlumno;
+            sexoNotaMayor = sexoAlumno;
+        } else if (notaMenor > notaAlumno) {
+            notaMenor = notaAlumno;
+            sexoNotaMenor = sexoAlumno;
+        }
+            
+        if (notaAlumno >= 6 && sexoAlumno.toLowerCase() == "m") {
+            contadorVaronesNotaMayor++;
+        }
+
+        //acumulador notas
+        acumuladorNotas += notaAlumno; 
     }
+    
+    //operacion promedio
+    promedioNotas = acumuladorNotas / contador;
 
-
+    //mostrar
+    document.write(" A) El promedio de las notas totales: " + promedioNotas + "<br>" + 
+                   " B) La nota más baja y el sexo de esa persona: " + notaMenor + " " + sexoNotaMenor + "<br>" +
+                   "    La nota más alta y el sexo de esa persona: " + notaMayor + " " + sexoNotaMayor + "<br>" +
+                   " C) La cantidad de varones que su nota haya sido mayor o igual a 6: " + contadorVaronesNotaMayor);
 }
