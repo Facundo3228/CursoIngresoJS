d0900bc973d8cc372183fa49834ca017517dc01a
Facundo3228 <esteban-r-m@hotmail.es>
2020-02-27 17:03:53 -0300

    Trabajo en casa

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,50 @@
+/**
+ * Enunciado:
+Bienvenidos.
+Realizar el algoritmo que permita ingresar la marca del producto, el peso el cual debe ser entre 1 y 100
+(validar), la temperatura de almacenamiento(entre -30 y 30) hasta que el usuario quiera e informar al
+terminar el ingreso por document.write:
+
+a) La cantidad de temperaturas pares.
+b) La marca del producto más pesado
+c) La cantidad de productos que se conservan a menos de 0 grados.
+d) El promedio del peso de todos los productos.
+f) El peso máximo y el mínimo.
+
+Testeo con los siguientes valores
+(m=pepito;p = 50; t =-300(mal) ,-30(bien) )
+(m=teem;p = 10; t =0 )
+(m=llut;p = 150(mal), 15(bien); t =-13 )
+(m=fpy;p = 45; t =-12 )
+ */
 function mostrar()
 {
+    //variables y inicio
+    var marcaIngresada;
+    var pesoIngresado;
+    var temperaturaIngresada;
+
+    var contadorPeso = 0;
+
+
+
+    //ingreso de datos y validaciones
+    do {
+        marcaIngresada = prompt("Ingrese la marca ");
+    } while (! isNaN(marcaIngresada.toLowerCase)());
+
+    do {
+        pesoIngresado = prompt("Ingrese el peso entre 1kg a 100kg ");
+        pesoIngresado = parseInt(pesoIngresado);
+    } while (isNaN(pesoIngresado) || pesoIngresado < 0 || pesoIngresado > 101);
+
+    contadorPeso++; // cuento la cantidad de pesos ingresados para sacar el promedio
+
+    do {
+        temperaturaIngresada = prompt("Ingrese la temperatura de lmacenamiento entre -30º a 30º ");
+        temperaturaIngresada = parseInt(temperaturaIngresada);
+    } while (isNaN(temperaturaIngresada) || temperaturaIngresada < -30 || temperaturaIngresada > 30);
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -28,45 +28,95 @@ function mostrar()
     var cantidadNumerosPares = 0;
     var cantidadNumerosImpares = 0;
     var cantidadCeros = 0;
-    var numeroPositivo;
+    var acumuladorNumeroPositivo = 0;
     var promedio;
-    var acumuladorNumeroNegativos;
+    var contadorNumeroPositivo = 0;
+    var acumuladorNumeroNegativos = 0;
     var numeroMaximo;
     var numeroMinimo;
     var letraMaximo;
     var letraMinimo;
 
+    //varibles de control y inicio 
     var bandera = true;
     var respuesta = true;
+    var mensaje;
 
     while (respuesta) {
 
         do {
-            letraIngresada = prompt("Ingrese una letra: ");      
-        } while (!(isNaN(letraIngresada.toLowerCase())));
+            letraIngresada = prompt("Ingrese una letra: "); 
+            /**
+             * Nota personal: para validar una letra podemos usar el isNaN() negando el valor y para restringir la
+             * cantidad de caracteres podemos usar la propiedad length-1 igualando la cantidad de caracteres
+             * que queremos menos 1.
+            */     
+        } while ((! isNaN(letraIngresada.toLowerCase())) || letraIngresada.toLowerCase().length-1 != 0);
 
         do {
             numeroIngresado = prompt("Ingrese un numero entre -100 y 100: ");
             numeroIngresado = parseInt(numeroIngresado);
         } while (isNaN(numeroIngresado) || numeroIngresado < -100 || numeroIngresado > 100);
-
         
         //f) El número y la letra del máximo y el mínimo.
         if (bandera) {
             bandera = false;
-            var numeroMaximo = numeroIngresado;
-            var numeroMinimo = numeroIngresado;
-            var letraMaximo = letraIngresada;
-            var letraMinimo ; 
-            
+            numeroMaximo = numeroIngresado; // 20  // 20  // 20 // 30 
+            numeroMinimo = numeroIngresado; // 20  // 10  // 4  // 4
+            letraMaximo = letraIngresada; // l  // l  // l  // k
+            letraMinimo = letraIngresada; // l  // r  // g // g
+        } else {
+            //????
+            if (numeroIngresado > numeroMaximo) {
+                numeroMaximo = numeroIngresado;
+                letraMaximo = letraIngresada;
+            } else {
+                //¿Debo declarar la condicion?  <-----
+                numeroMinimo = numeroIngresado;
+                letraMinimo = letraIngresada;
+            }
         }
 
+        //e) La suma de todos los números negativos.
+        //d) El promedio de todos los números positivos ingresados.
+        if (numeroIngresado < 0) {
+            acumuladorNumeroNegativos += numeroIngresado;
+        } else {
+            acumuladorNumeroPositivo += numeroIngresado;
+            contadorNumeroPositivo++;
+        }
 
-
-
-
+        //c) La cantidad de ceros ¡Atencion el cero es neutro!.
+        //a) La cantidad de números pares. 
+        //b) La cantidad de números impares. 
+        if (numeroIngresado == 0) {
+            cantidadCeros++;
+        } else {
+            if (numeroIngresado %2 == 0) {
+                cantidadNumerosPares++;
+            } else {
+                cantidadNumerosImpares++;
+            }
+        }
 
         respuesta = confirm("¿QUIERE CONTINUAR?");
     }
 
+    //operatoria promedio y validacion
+    if (contadorNumeroPositivo > 0) {
+        promedio = acumuladorNumeroPositivo / contadorNumeroPositivo;        
+    } else {
+        mensaje = "¡¡¡ NO SE PUEDE DIVIDIR POR 0 !!! USTED NO INGRESO NINGUN NUMERO POSITIVO...";
+        promedio = mensaje;
+    }
+    
+    //muestro por document.write()
+    document.write("a) La cantidad de números pares: " + cantidadNumerosPares + "<br>" +
+                   "b) La cantidad de números impares: " + cantidadNumerosImpares + "<br>" +
+                   "c) La cantidad de ceros ¡Atencion el cero es neutro!: " + cantidadCeros + "<br>" +
+                   "d) El promedio de todos los números positivos ingresados." + promedio + "<br>" +
+                   "e) La suma de todos los números negativos: " + acumuladorNumeroNegativos + "<br>" +
+                   "f) El número y la letra del máximo y el mínimo: <br>" +
+                   "Letra y numero maximo: " + letraMaximo + "-" + numeroMaximo + "<br>" +
+                   "Letra y numero minimo: " + letraMinimo + "-" + numeroMinimo);
 }
